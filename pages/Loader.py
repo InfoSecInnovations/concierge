f5964f3b8ed83eec06f2128d1d2a98615bfcc29a
Sebastian Boutin Blomfield <7989595+sebovzeoueb@users.noreply.github.com>
2024-02-14 20:10:52 +0100

    improvements to data flow in loader

--- pages/Loader.py
+++ pages/Loader.py
@@ -34,18 +34,39 @@ def add_url():
     if url:
         st.session_state["input_urls"].append(url)
 
-st.write('# Document Loader')
+def ingest():
+    files = st.session_state[st.session_state["file_uploader_key"]]
+    if files and len(files):
+        with st.session_state["loader_container"].container():
+            st.write('Processing files...')
+            for file in files:
+                if file.type == 'application/pdf':
+                    print(file.name)
+                    with open(Path(upload_dir, file.name), "wb") as f:
+                        f.write(file.getbuffer())
+                    pages = LoadPDF(upload_dir, file.name)
+                    console_page_progress = tqdm(total=len(pages))
+                    progress_text = f"Loading PDF {file.name}"
+                    page_progress = st.progress(0, text=progress_text)
+                    for x in Insert(pages, collection):
+                        console_page_progress.n = x[0] + 1
+                        console_page_progress.refresh()
+                        page_progress.progress((x[0] + 1) / x[1], text=progress_text)
+                    page_progress.empty()
+                    console_page_progress.close()
+        print('done loading files\n')
+        st.session_state["file_uploader_key"] += 1
 
-if "files" in st.session_state:
-    st.write('Processing files...')
-    for file in st.session_state["files"]:
-            if file.type == 'application/pdf':
-                print(file.name)
-                with open(Path(upload_dir, file.name), "wb") as f:
-                    f.write(file.getbuffer())
-                pages = LoadPDF(upload_dir, file.name)
+    if len(st.session_state["input_urls"]):
+        with st.session_state["loader_container"].container():
+            st.write('Processing URLs...')
+            for url in st.session_state["input_urls"]:
+                if not url:
+                    continue
+                print(url)
+                pages = LoadWeb(url)
                 console_page_progress = tqdm(total=len(pages))
-                progress_text = f"Loading PDF {file.name}"
+                progress_text = f"Loading URL {url}"
                 page_progress = st.progress(0, text=progress_text)
                 for x in Insert(pages, collection):
                     console_page_progress.n = x[0] + 1
@@ -53,42 +74,14 @@ if "files" in st.session_state:
                     page_progress.progress((x[0] + 1) / x[1], text=progress_text)
                 page_progress.empty()
                 console_page_progress.close()
-    print('done loading files\n')
-    del st.session_state["files"]
-    st.rerun()
+        print('done loading URLs\n')
+        st.session_state["input_urls"] = []
 
-elif "urls" in st.session_state:
-    st.write('Processing URLs')
-    for url in st.session_state["urls"]:
-        if not url:
-            continue
-        print(url)
-        pages = LoadWeb(url)
-        console_page_progress = tqdm(total=len(pages))
-        progress_text = f"Loading URL {url}"
-        page_progress = st.progress(0, text=progress_text)
-        for x in Insert(pages, collection):
-            console_page_progress.n = x[0] + 1
-            console_page_progress.refresh()
-            page_progress.progress((x[0] + 1) / x[1], text=progress_text)
-        page_progress.empty()
-        console_page_progress.close()
-    print('done loading URLs\n')
-    del st.session_state["urls"]
-    st.rerun()
-
-else:
-    files = st.file_uploader(label='Select files to add to database', accept_multiple_files=True, key=st.session_state["file_uploader_key"])
-    st.write('### URLs ###')
-    for index, url in enumerate(st.session_state["input_urls"]):
-        st.session_state["input_urls"][index] = st.text_input("URL", url, label_visibility="collapsed", key=f"input_url_{index}")
-    st.text_input("URL", "", label_visibility="collapsed", key=f'input_url_{len(st.session_state["input_urls"])}', on_change=add_url)
-
-    if st.button(label='Ingest'):
-        if files and len(files):
-            st.session_state["files"] = files
-            st.session_state["file_uploader_key"] += 1
-        if len(st.session_state["input_urls"]):
-            st.session_state["urls"] = st.session_state["input_urls"].copy()
-            st.session_state["input_urls"] = []
-        st.rerun()
+st.write('# Document Loader')
+st.session_state["loader_container"] = st.empty()
+st.file_uploader(label='Select files to add to database', accept_multiple_files=True, key=st.session_state["file_uploader_key"])
+st.write('### URLs ###')
+for index, url in enumerate(st.session_state["input_urls"]):
+    st.session_state["input_urls"][index] = st.text_input("URL", url, label_visibility="collapsed", key=f"input_url_{index}")
+st.text_input("URL", "", label_visibility="collapsed", key=f'input_url_{len(st.session_state["input_urls"])}', on_change=add_url)
+st.button(label='Ingest', on_click=ingest)
