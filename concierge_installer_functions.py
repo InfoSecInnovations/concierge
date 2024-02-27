8ff9cf37b385e308f2c9605c3ede43c06030a57c
Sebastian Boutin Blomfield <7989595+sebovzeoueb@users.noreply.github.com>
2024-02-27 20:53:28 +0100

    implement missing functionality in install_new.py

---
+++ concierge_installer_functions.py
@@ -0,0 +1,8 @@
+import subprocess
+import os
+
+def docker_compose_helper(compute_method):
+    if compute_method == "GPU":
+        subprocess.run(["docker", "compose", "-f", os.path.abspath("docker-compose-gpu.yml"), "up", "-d"])
+    else:
+        subprocess.run(["docker", "compose", "up", "-d"])
--- script_builder/util.py
+++ script_builder/util.py
@@ -1,6 +1,8 @@
 import platform
 import os
 import ctypes
+import subprocess
+import venv
 
 my_platform = platform.system()
 
@@ -17,6 +19,17 @@ def disallow_admin():
             exit()
     # TODO: macOS
 
+def require_admin():
+    if my_platform == "Linux":
+        if os.geteuid() != 0:
+            print("Please run this script as root or with sudo.")
+            exit()
+    if my_platform == "Windows":
+        if ctypes.windll.shell32.IsUserAnAdmin() == 0:
+            print("Please run this script as administrator")
+            exit()
+    # TODO: macOS
+
 def get_default_directory(is_standalone: bool):
     if my_platform == "Linux":
         if is_standalone:
@@ -25,5 +38,17 @@ def get_default_directory(is_standalone: bool):
     if my_platform == "Windows":
         if is_standalone:
             return os.getenv('LOCALAPPDATA')
-        return "C:\ProgramData"
+        return r"C:\ProgramData"
     # TODO: macOS
+
+def pip_loader():
+    working_dir = os.getcwd()
+    print(working_dir)
+    venv.create(working_dir, with_pip=True)
+    # pip install command
+    if my_platform == "Linux":
+        dir = "bin"
+    else:
+        dir = "Scripts"
+    # TODO: macOS
+    subprocess.run([os.path.join(dir, "python"), "-m", "pip", "install", "-r", os.path.abspath("requirements.txt")], cwd=working_dir)
