5af0b9eef831459a12e7e57da24dbf10747b72b4
Sebastian Boutin Blomfield <7989595+sebovzeoueb@users.noreply.github.com>
2024-03-07 21:10:43 +0100

    update CLI web loader to take collection argument

--- cli/web-loader.py
+++ cli/web-loader.py
@@ -4,11 +4,14 @@ from concierge_backend_lib.ingesting import InsertWithTqdm
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('url', required=True, help="Required: the URL of the website you wish to scrape.")
+parser.add_argument("-u", "--url", required=True, 
+                    help="Required: the URL of the website you wish to scrape.")
+parser.add_argument("-c", "--collection", required=True,
+                    help="Milvus collection containing the vectorized data.")
 args = parser.parse_args()
 url = args.url
 
-collection = InitCollection("facts")
+collection = InitCollection(args.collection)
 
 pages = LoadWeb(url)
 print (url)
