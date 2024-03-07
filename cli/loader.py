3de45922639a0d872c896cf3071b0c1b918acb5d
Sebastian Boutin Blomfield <7989595+sebovzeoueb@users.noreply.github.com>
2024-03-07 21:08:24 +0100

    make CLI loader compatible with web UI

--- cli/loader.py
+++ cli/loader.py
@@ -3,6 +3,9 @@ from loaders.pdf import LoadPDF
 from concierge_backend_lib.collections import InitCollection
 from concierge_backend_lib.ingesting import InsertWithTqdm
 import argparse
+import shutil
+
+upload_dir = os.path.join('..', 'uploads')
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--source", required=True, 
@@ -16,9 +19,12 @@ source_files = os.listdir(source_path)
 collection = InitCollection(args.collection)
 
 for file in source_files:
+    shutil.copyfile(os.path.join(source_path, file), os.path.join(upload_dir, file))
     print(file)
     pages = None
     if file.endswith(".pdf"):
-        pages = LoadPDF(source_path, file)
+        pages = LoadPDF(upload_dir, file)
     if pages:
         InsertWithTqdm(pages, collection)
+
+collection.flush() # if we don't flush, the Web UI won't be able to grab recent changes
