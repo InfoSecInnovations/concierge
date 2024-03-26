d7e6326bae3af194ead30341e5837993c26322d6
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2024-03-27 00:07:30 +0100

    WIP async loader

--- concierge-shiny/modules.py
+++ concierge-shiny/modules.py
@@ -1,6 +1,7 @@
 from shiny import ui, reactive, render, Inputs, Outputs, Session, module
 import shutil
 import os
+import asyncio
 from tqdm import tqdm
 from concierge_backend_lib.ingesting import insert
 from loaders.pdf import load_pdf
@@ -40,33 +41,60 @@ def home_ui():
 def loader_ui():
     return [
         ui.markdown("# Loader"),
-        ui.output_ui("ingester"),
-        ui.input_action_button(id="ingest", label="Ingest")
+        ui.output_ui("file_input"),
+        ui.input_task_button(id="ingest", label="Ingest")
     ]
 
 @module.server
 def loader_server(input: Inputs, output: Outputs, session: Session, collection, upload_dir):
-    @render.ui
-    @reactive.event(input.ingest, ignore_none=False, ignore_init=False)
-    def ingester():
+
+    def ingest_files(files):
+        for file in files:
+            shutil.copyfile(file["datapath"], os.path.join(upload_dir, file["name"]))
+            if file["type"] == 'application/pdf':
+                print(file["name"])
+                pages = load_pdf(upload_dir, file["name"])
+                page_progress = tqdm(total=len(pages))
+                with ui.Progress(1, len(pages)) as p:
+                    p.set(0, message=f"{file["name"]}: loading...")
+                    for x in insert(pages, collection):
+                        p.set(x[0] + 1, message=f"{file["name"]}: page {x[0] + 1} of {x[1]}.")
+                        page_progress.n = x[0] + 1
+                        page_progress.refresh()
+                page_progress.close()
+
+    @ui.bind_task_button(button_id="ingest")
+    @reactive.extended_task
+    async def ingester_async(files):
+        await asyncio.to_thread(ingest_files(files))
+
+    @reactive.effect
+    @reactive.event(input.ingest, ignore_none=False)
+    def handle_click():
         if "loader_files" in input:
+            print("yoyo")
             files = input.loader_files()
             if files and len(files):
-                for file in files:
-                    shutil.copyfile(file["datapath"], os.path.join(upload_dir, file["name"]))
-                    if file["type"] == 'application/pdf':
-                        print(file["name"])
-                        pages = load_pdf(upload_dir, file["name"])
-                        page_progress = tqdm(total=len(pages))
-                        with ui.Progress(1, len(pages)) as p:
-                            p.set(0, message=f"{file["name"]}: loading...")
-                            for x in insert(pages, collection):
-                                p.set(x[0] + 1, message=f"{file["name"]}: page {x[0] + 1} of {x[1]}.")
-                                page_progress.n = x[0] + 1
-                                page_progress.refresh()
-                        page_progress.close()
+                print(len(files))         
+                ingester_async(files)
+
+    @render.ui
+    def file_input():
         return ui.input_file(
             id="loader_files",
             label="Documents",
             multiple=True
         )
+
+    # @render.ui
+    # @reactive.event(input.ingest, ignore_none=False, ignore_init=False)
+    # def ingester():
+    #     if "loader_files" in input:
+    #         files = input.loader_files()
+    #         if files and len(files):
+    #             ingest_files(files)
+    #     return ui.input_file(
+    #         id="loader_files",
+    #         label="Documents",
+    #         multiple=True
+    #     )
