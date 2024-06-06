37e20d2be45449aae1f17e06c601ff6940cee8ca
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2024-06-06 13:38:29 +0200

    fix OpenSearch status getter

--- concierge_backend_lib/status.py
+++ concierge_backend_lib/status.py
@@ -1,20 +1,22 @@
 import requests
 import os
-from concierge_backend_lib.opensearch import get_client
+from concierge_backend_lib.opensearch import get_client, get_indices
 
-HOST = os.getenv("OLLAMA_HOST") or "localhost"
+OLLAMA_HOST = os.getenv("OLLAMA_HOST") or "localhost"
+OPENSEARCH_HOST = os.getenv("OPENSEARCH_HOST") or "localhost"
 
 
 def check_ollama():
     try:
-        return requests.get(f"http://{HOST}:11434/").status_code == 200
+        return requests.get(f"http://{OLLAMA_HOST}:11434/").status_code == 200
     except Exception:
         return False
 
 
 def check_opensearch():
     try:
-        get_client()
+        client = get_client()
+        get_indices(client)
         return True
     except Exception:
         return False
