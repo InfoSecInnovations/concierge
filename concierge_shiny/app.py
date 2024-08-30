4c041dabadd4a15839d777d052a73e348794054e
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2024-08-30 10:50:35 +0200

    move auth into separate function

--- concierge_shiny/app.py
+++ concierge_shiny/app.py
@@ -2,7 +2,7 @@ from shiny import App, ui, Inputs, Outputs, Session, reactive, render
 from home import home_ui
 from prompter import prompter_ui, prompter_server
 from collection_management import collection_management_ui, collection_management_server
-from concierge_backend_lib.opensearch import get_collections, get_client
+from concierge_backend_lib.opensearch import get_collections
 import shinyswatch
 from components import status_ui, status_server
 from isi_util.async_single import asyncify
@@ -10,12 +10,9 @@ from opensearch_binary import serve_binary
 from oauth2 import auth_callback, refresh
 from starlette.applications import Starlette
 from starlette.routing import Mount, Route
-from oid_configs import oauth_configs, oauth_config_data
-import json
-from requests_oauthlib import OAuth2Session
-from oauthlib.oauth2 import TokenExpiredError
 import os
 import dotenv
+from functions import get_authorized_client
 
 dotenv.load_dotenv()
 
@@ -30,58 +27,10 @@ app_ui = ui.page_auto(
 
 
 def server(input: Inputs, output: Outputs, session: Session):
-    if (
-        "concierge_token_chunk_count" not in session.http_conn.cookies
-        or "concierge_auth_provider" not in session.http_conn.cookies
-    ):
-        urls = []
-        redirect_uri = f"{session.http_conn.headers["origin"]}/callback/"
-
-        for provider, data in oauth_config_data.items():
-            config = oauth_configs[provider]
-            oauth = OAuth2Session(
-                client_id=os.getenv(data["id_env_var"]),
-                redirect_uri=redirect_uri + provider,
-                scope=scope,
-            )
-            authorization_url, state = oauth.authorization_url(
-                config["authorization_endpoint"]
-            )
-            urls.append(authorization_url)
-
-        @render.ui
-        def concierge_main():
-            return ui.markdown("\n\n".join([f"<{url}>" for url in urls]))
-
+    shinyswatch.theme_picker_server()
+    client, token = get_authorized_client(session)
+    if not client:
         return
-
-    # TODO: only do this if security is enabled
-
-    chunk_count = int(session.http_conn.cookies["concierge_token_chunk_count"])
-    token = ""
-    for i in range(chunk_count):
-        token += session.http_conn.cookies[f"concierge_auth_{i}"]
-    provider = session.http_conn.cookies["concierge_auth_provider"]
-    oidc_config = oauth_configs[provider]
-    data = oauth_config_data[provider]
-    parsed_token = json.loads(token)
-    oauth = OAuth2Session(client_id=os.getenv(data["id_env_var"]), token=parsed_token)
-    try:
-        oauth.get(
-            oidc_config["userinfo_endpoint"]
-        ).json()  # TODO: maybe do something with the user info?
-    except TokenExpiredError:
-
-        @render.ui
-        def concierge_main():
-            return ui.tags.script(f'window.location.href = "/refresh/{provider}"')
-
-        return
-
-    client = get_client(parsed_token["id_token"])
-
-    # TODO: if security isn't enabled make basic get_client call
-
     opensearch_status = reactive.value(False)
     ollama_status = reactive.value(False)
     selected_collection = reactive.value("")
@@ -115,10 +64,7 @@ def server(input: Inputs, output: Outputs, session: Session):
         opensearch_status,
         client,
     )
-    status = status_server(
-        "status_widget", parsed_token["id_token"]
-    )  # TODO: optional token
-    shinyswatch.theme_picker_server()
+    status = status_server("status_widget", token)  # TODO: optional token
 
     @reactive.extended_task
     async def set_collections():
