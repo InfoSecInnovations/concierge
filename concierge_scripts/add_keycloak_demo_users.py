a408756ad54fc28cabae63b6e3cfd6a48f513461
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2024-11-01 19:58:24 +0100

    fix demo user script

--- concierge_scripts/add_keycloak_demo_users.py
+++ concierge_scripts/add_keycloak_demo_users.py
@@ -10,7 +10,17 @@ def add_users():
         users_data = json.load(file)
     client = get_keycloak_admin_client()
     for user in users_data:
-        client.create_user(user)
+        user_id = client.create_user(user)
+        # creating with roles unfortunately doesn't work even though it's implied in the documentation1
+        # so we have to add the mapping separately
+        if "clientRoles" in user:
+            for client_name, roles in user["clientRoles"].items():
+                client_id = client.get_client_id(client_name)
+                client.assign_client_role(
+                    user_id,
+                    client_id,
+                    [client.get_client_role(client_id, role) for role in roles],
+                )
         print(f"Added user {user["username"]}")
 
 
