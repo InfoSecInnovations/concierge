ff025717309c4b8f732326fcc37873af714ff85c
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2024-11-29 18:56:16 +0100

    implement docker service checks and removals for all services

--- concierge_packages/launcher/src/launch_concierge/concierge_installer/docker_containers.py
+++ concierge_packages/launcher/src/launch_concierge/concierge_installer/docker_containers.py
@@ -23,5 +23,38 @@ def container_exists(container_name):
         return False
 
 
+def remove_container(container_name):
+    subprocess.run(["docker", "container", "rm", "--force", container_name])
+
+
+def remove_volume(volume_name):
+    subprocess.run(["docker", "volume", "rm", "--force", f"concierge_{volume_name}"])
+
+
 def keycloak_exists():
     return container_exists("keycloak") and container_exists("postgres")
+
+
+def remove_keycloak():
+    remove_container("keycloak")
+    remove_container("postgres")
+    remove_volume("postgres_data")
+
+
+def ollama_exists():
+    return container_exists("ollama")
+
+
+def remove_ollama():
+    remove_container("ollama")
+    remove_volume("ollama")
+
+
+def opensearch_exists():
+    return container_exists("opensearch-node1")
+
+
+def remove_opensearch():
+    remove_container("opensearch-node1")
+    remove_container("opensearch-dashboards")
+    remove_volume("opensearch-data1")
