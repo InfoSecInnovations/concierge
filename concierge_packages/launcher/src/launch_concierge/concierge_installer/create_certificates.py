ec2a945b12c2d19af03d8e1d719794e2b9942907
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2024-12-10 09:26:51 +0100

    create all required certificates

--- concierge_packages/launcher/src/launch_concierge/concierge_installer/create_certificates.py
+++ concierge_packages/launcher/src/launch_concierge/concierge_installer/create_certificates.py
@@ -62,6 +62,15 @@ def create_certificates(cert_dir):
     with open(os.path.join(cert_dir, "root-ca.pem"), "wb") as f:
         f.write(root_cert.public_bytes(serialization.Encoding.PEM))
 
+    with open(os.path.join(cert_dir, "root-ca-key.pem"), "wb") as f:
+        f.write(
+            root_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption(),
+            )
+        )
+
     def create_signed_cert(alt_names, cert_name):
         ee_key = rsa.generate_private_key(
             public_exponent=65537,
@@ -73,6 +82,7 @@ def create_certificates(cert_dir):
                 x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "Ohio"),
                 x509.NameAttribute(NameOID.LOCALITY_NAME, "Dublin"),
                 x509.NameAttribute(NameOID.ORGANIZATION_NAME, "Self Sign"),
+                x509.NameAttribute(NameOID.COMMON_NAME, cert_name),
             ]
         )
         ee_cert = (
@@ -149,4 +159,44 @@ def create_certificates(cert_dir):
             x509.DNSName("keycloak"),
         ],
         "keycloak",
+    )
+
+    create_signed_cert(
+        [
+            x509.DNSName("localhost"),
+            x509.DNSName("ollama"),
+        ],
+        "ollama",
+    )
+
+    create_signed_cert(
+        [
+            x509.DNSName("localhost"),
+            x509.DNSName("concierge"),
+        ],
+        "concierge",
+    )
+
+    create_signed_cert(
+        [
+            x509.DNSName("localhost"),
+            x509.DNSName("opensearch-node1"),
+        ],
+        "opensearch-node1",
+    )
+
+    create_signed_cert(
+        [
+            x509.DNSName("localhost"),
+            x509.DNSName("opensearch-dashboards"),
+        ],
+        "opensearch-dashboards",
+    )
+
+    create_signed_cert(
+        [
+            x509.DNSName("localhost"),
+            x509.DNSName("opensearch-node1"),
+        ],
+        "opensearch-admin",
     )
