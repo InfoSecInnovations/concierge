a486f295f96c8dc9518ec61f9a7f3ec91b74f7c4
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-01-07 23:01:56 +0100

    add tests for get_install_status

--- concierge_packages/script_builder/tests/test_util.py
+++ concierge_packages/script_builder/tests/test_util.py
@@ -1,4 +1,5 @@
-from script_builder.util import get_venv_executable, is_unix
+from script_builder.util import get_venv_executable, is_unix, get_install_status
+import pytest
 
 
 def test_get_venv_executable():
@@ -7,3 +8,19 @@ def test_get_venv_executable():
         assert path == "bin/python"
     else:
         assert path == "Scripts\\python"
+
+
+def test_get_install_status(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "yes")
+    assert get_install_status("yes", "no")
+
+
+def test_get_negative_install_status(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "no")
+    with pytest.raises(SystemExit):
+        get_install_status("yes", "no")
+
+
+def test_get_neutral_install_status(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "blah")
+    assert not get_install_status("yes", "no")
