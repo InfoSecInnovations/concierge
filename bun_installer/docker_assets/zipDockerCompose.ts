a4b1ebb253f7b291fc779d5339cbeb75c74e56c4
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-02-20 12:35:14 +0100

    use sync versions of zipping functions

--- bun_installer/docker_assets/zipDockerCompose.ts
+++ bun_installer/docker_assets/zipDockerCompose.ts
@@ -2,5 +2,5 @@ import AdmZip from "adm-zip";
 import path from "node:path"
 
 const zip = new AdmZip()
-await zip.addLocalFolderPromise(path.join(import.meta.dir, "docker_compose"), {zipPath: "docker_compose"})
-await zip.writeZipPromise(path.join(import.meta.dir, "..", "assets", "docker_compose.zip"), {overwrite: true})
+zip.addLocalFolder(path.join(import.meta.dir, "docker_compose"), "docker_compose")
+zip.writeZip(path.join(import.meta.dir, "..", "assets", "docker_compose.zip"))
