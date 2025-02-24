6ae3beb29271c5f18d56fafd90109fc8a4f27d1c
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-02-24 23:19:07 +0100

    increment version to 0.5a17

--- bun_installer/publish.ts
+++ bun_installer/publish.ts
@@ -31,7 +31,8 @@ const handlePyPi = async (packageUrlName: string, packageName: string, tomlData:
     await $`rm -rf ${path.join(packageDir, "dist")}`
     console.log(packageDir)
     await exec("python3 -m build", {cwd: packageDir})
-    await runPython(`twine upload ${path.join(packageDir, "dist")}/* -u __token__ -p ${pyPiKey}`)
+    // single backslashes get stripped out by the command line so we need to double them
+    await runPython(`twine upload ${path.join(packageDir, "dist").replace("\\", "\\\\")}/* -u __token__ -p ${pyPiKey}`)
 }
 await handlePyPi("concierge-util", "concierge_util", conciergeUtilPyProject)
 await handlePyPi("isi-util", "isi_util", isiUtilPyProject)
