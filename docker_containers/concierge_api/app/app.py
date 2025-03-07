b5be48338ca5b7221bfde9b428fd721acdc4d8cb
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-03-07 19:39:49 +0100

    WIP implement collection API routes

--- docker_containers/concierge_api/app/app.py
+++ docker_containers/concierge_api/app/app.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
-from document_collections import create_collection, get_collections
+from fastapi import FastAPI, UploadFile
+from document_collections import (
+    create_collection,
+    get_collections,
+    delete_collection,
+    get_documents,
+    delete_document,
+)
+from ingesting import insert_document
 from authorization import auth_enabled
+from loading import load_file
+import tempfile
 
 app = FastAPI()
 
@@ -9,9 +18,38 @@ app = FastAPI()
 if not auth_enabled():
 
     @app.post("/collections")
-    async def post_create_collection(name: str):
+    async def create_collection_route(name: str):
         return await create_collection(None, name)
 
     @app.get("/collections")
-    async def list_collections():
+    async def get_collections_route():
         return await get_collections(None)
+
+    @app.delete("/collections/{collection_id}")
+    async def delete_collection_route(collection_id: str):
+        return await delete_collection(None, collection_id)
+
+    @app.get("/collections/{collection_id}/documents")
+    async def get_documents_route(collection_id: str):
+        return await get_documents(None, collection_id)
+
+    @app.post("/collections/{collection_id}/documents/files")
+    async def insert_files_document_route(collection_id: str, files: list[UploadFile]):
+        for file in files:
+            with tempfile.NamedTemporaryFile(suffix=file.filename, delete=False) as fp:
+                fp.write(file.file.read())
+                file_path = fp.name
+            doc = load_file(file_path)
+            async for result in insert_document(None, collection_id, doc):
+                pass
+
+    @app.post("/collections/{collection_id}/documents/urls")
+    async def insert_url_document_route(collection_id: str, urls: list[str]):
+        # TODO: loader
+        return await insert_document(None, collection_id)
+
+    @app.delete("/collections/{collection_id}/documents/{document_type}/{document_id}")
+    async def delete_document_route(
+        collection_id: str, document_type: str, document_id: str
+    ):
+        return await delete_document(None, collection_id, document_type, document_id)
