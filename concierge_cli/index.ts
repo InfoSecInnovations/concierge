9058d2789ceb993a168742147a744a2341a3a10c
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-05-01 18:35:47 +0200

    add file ingesting to the CLI

--- concierge_cli/index.ts
+++ concierge_cli/index.ts
@@ -2,6 +2,8 @@ import * as commander from 'commander';
 import getAuthClient from './getAuthClient';
 import * as dotenv from 'dotenv';
 import path from 'node:path'
+import { readdir } from 'node:fs/promises';
+import * as cliProgress from 'cli-progress';
 
 // we set this to 'executable' when building the standalone
 const environment = process.env.CONCIERGE_ENVIRONMENT_TYPE || 'local';
@@ -37,6 +39,24 @@ if (authEnabled) {
       collections.forEach(collection => console.log(collection))
     })
   ))
+}
+
+const ingest = program.command('ingest')
+if (authEnabled) {
+  ingest.command('directory <directory>')
+  .option('-c, --collection <collection>', 'collection id to ingest into')
+  .action(async (directory, options) => {
+    const files = await readdir(directory, { withFileTypes: true, recursive: true });
+    const actualFiles = files.filter(file => file.isFile());
+    const client = await getAuthClient();
+    const bar = new cliProgress.SingleBar({format: '{bar} {value}/{total} chunks {label}'}, cliProgress.Presets.shades_classic);
+    bar.start(0, 0, {label: 'Ingesting files...'});
+    for await (const item of await client.insertFiles(options.collection, actualFiles.map(file => path.join(file.parentPath, file.name)))) {
+      bar.setTotal(item.total);
+      bar.update(item.progress + 1, {label: item.label}); // progress is 0 indexed but the bar is 1 indexed
+    }
+    bar.stop();
+  })
 }
 
 program.parse(Bun.argv)
