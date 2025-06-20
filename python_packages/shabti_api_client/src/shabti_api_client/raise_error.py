72db42a438aad9cc6b4d389e46940b1f5f0c3bdd
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-06-20 12:19:50 +0200

    add exception handler to Shabti API client error raising function

--- python_packages/shabti_api_client/src/shabti_api_client/raise_error.py
+++ python_packages/shabti_api_client/src/shabti_api_client/raise_error.py
@@ -4,10 +4,15 @@ from shabti_types import CollectionExistsError
 
 
 def raise_error(response: Response):
-    body = response.json()
-    if response.status_code == 500:
-        if "error_type" in body:
-            if body["error_type"] == "CollectionExistsError":
-                raise CollectionExistsError
-
-    raise ShabtiRequestError(status_code=response.status_code, message=body)
+    try:
+        body = response.json()
+        if response.status_code == 500:
+            if "error_type" in body:
+                if body["error_type"] == "CollectionExistsError":
+                    raise CollectionExistsError
+        raise ShabtiRequestError(status_code=response.status_code, message=body)
+    except Exception:
+        raise ShabtiRequestError(
+            status_code=response.status_code,
+            message=f"Status code: {response.status_code}",
+        )
