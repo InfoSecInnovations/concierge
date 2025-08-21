9e68c6089ad7c8de69ae631b48c9890da310446e
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-08-21 15:26:17 +0200

    install unstructured requirements into docker image

--- docker_containers/shabti_api/src/loaders/unstructured.py
+++ docker_containers/shabti_api/src/loaders/unstructured.py
@@ -2,6 +2,7 @@ from .base_loader import ShabtiFileLoader, ShabtiDocument, get_current_time
 from langchain_unstructured.document_loaders import UnstructuredLoader
 from pathlib import Path
 import os
+import traceback
 
 
 class UnstructuredFileLoader(ShabtiFileLoader):
@@ -11,25 +12,29 @@ class UnstructuredFileLoader(ShabtiFileLoader):
 
     @staticmethod
     def load(full_path: str, filename: str | None) -> ShabtiDocument:
-        date_time = get_current_time()
-        loader = UnstructuredLoader(file_path=full_path, strategy="fast")
-        pages = loader.load()
-        return ShabtiDocument(
-            metadata=ShabtiFileLoader.FileMetaData(
-                type="file",
-                source=full_path,
-                filename=filename or Path(full_path).name,
-                extension=os.path.splitext(full_path)[1],
-                ingest_date=date_time,
-                media_type=pages[0].metadata["filetype"]
-                if len(pages) and "filetype" in pages[0].metadata
-                else None,
-            ),
-            pages=[
-                ShabtiDocument.ShabtiPage(
-                    metadata=ShabtiDocument.ShabtiPage.PageMetadata(),  # TODO: use page_number from metadata, there's other good stuff in the metadata too!
-                    content=page.page_content,
-                )
-                for page in pages
-            ],
-        )
+        try:
+            date_time = get_current_time()
+            loader = UnstructuredLoader(file_path=full_path, strategy="fast")
+            pages = loader.load()
+            return ShabtiDocument(
+                metadata=ShabtiFileLoader.FileMetaData(
+                    type="file",
+                    source=full_path,
+                    filename=filename or Path(full_path).name,
+                    extension=os.path.splitext(full_path)[1],
+                    ingest_date=date_time,
+                    media_type=pages[0].metadata["filetype"]
+                    if len(pages) and "filetype" in pages[0].metadata
+                    else None,
+                ),
+                pages=[
+                    ShabtiDocument.ShabtiPage(
+                        metadata=ShabtiDocument.ShabtiPage.PageMetadata(),  # TODO: use page_number from metadata, there's other good stuff in the metadata too!
+                        content=page.page_content,
+                    )
+                    for page in pages
+                ],
+            )
+        except Exception as e:
+            traceback.print_exc()
+            raise e
