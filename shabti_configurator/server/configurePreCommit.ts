564212264aa5f009dc0d7ee3f4e201f161d916b1
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-09-03 11:13:31 +0200

    update install process to use uv

--- shabti_configurator/server/configurePreCommit.ts
+++ shabti_configurator/server/configurePreCommit.ts
@@ -1,7 +1,3 @@
-import { platform } from "node:os";
 import { $ } from "bun";
 
-export default () => {
-	if (platform() == "win32") return $`Scripts\\pre-commit install`.cwd("..");
-	return $`./bin/pre-commit install`.cwd("..");
-};
+export default () => $`uv run pre-commit install`.cwd("..");
--- shabti_configurator/server/createVenv.ts
+++ shabti_configurator/server/createVenv.ts
@@ -1,6 +1,5 @@
 import path from "node:path";
 import util from "node:util";
-import runPython from "./runPython";
 const exec = util.promisify(
 	await import("node:child_process").then(
 		(child_process) => child_process.exec,
@@ -9,20 +8,7 @@ const exec = util.promisify(
 
 export default async (pathSegments: string[] = []) => {
 	// some systems only have python3 as the executable and others only have python, just in case someone has a whole other configuration we'll log an error but continue installing
-	await exec("python3 -m venv .", {
+	await exec("uv sync", {
 		cwd: path.resolve(path.join("..", ...pathSegments)),
-	})
-		.catch(() =>
-			exec("python -m venv .", {
-				cwd: path.resolve(path.join("..", ...pathSegments)),
-			}),
-		)
-		.then(
-			() => runPython("pip install -r dev_requirements.txt", pathSegments),
-			() =>
-				console.error(`Python virtual environment was unable to be configured in ${path.resolve(path.join("..", ...pathSegments))}! 
-Please follow the instructions appropriate to your operating system and Python configuration to do so manually.
-Once the environment is configured you will need to install the dependencies from the dev_requirements.txt in this directory.
-`),
-		);
+	});
 };
