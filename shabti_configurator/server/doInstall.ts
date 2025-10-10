641ade571b59bd13a9c5758f57166a575ab192fd
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-10-10 15:51:06 +0200

    fix Keycloak launch command to work with latest version, fix error if env value is missing, pin PyTorch versions

--- shabti_configurator/server/doInstall.ts
+++ shabti_configurator/server/doInstall.ts
@@ -148,7 +148,7 @@ export default async function* (options: FormData, installVenv = true) {
 			if (envs.SHABTI_COMPUTE == "cuda") {
 				yield logMessage("Installing CUDA version of pytorch");
 				await runPython(
-					"pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126",
+					"pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu126",
 					["docker_containers", "shabti_api"],
 				);
 			}
