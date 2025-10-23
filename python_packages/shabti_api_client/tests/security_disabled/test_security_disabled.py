64aee052bfbb5303129c42c13f97e5ffb4bbd29b
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-10-23 09:47:04 +0200

    continue adding Python client tests

--- python_packages/shabti_api_client/tests/security_disabled/test_security_disabled.py
+++ python_packages/shabti_api_client/tests/security_disabled/test_security_disabled.py
@@ -1,13 +1,34 @@
 from isi_util.list_util import find
+import os
 
 collection_name = "test_collection"
 collection_lookup = {}
+document_id = None
+
+filename = "test_doc.txt"
+file_path = os.path.join(os.path.dirname(__file__), "..", "assets", filename)
 
 
 async def test_create_collection(shabti_client):
     collection_id = await shabti_client.create_collection(collection_name)
     assert collection_id
     collection_lookup[collection_name] = collection_id
+
+
+async def test_list_collections(shabti_client):
+    collections = await shabti_client.get_collections()
+    assert find(
+        collections, lambda x: x.collection_id == collection_lookup[collection_name]
+    )
+
+
+async def test_ingest_document(shabti_client):
+    global document_id
+    async for info in shabti_client.insert_files(
+        collection_lookup[collection_name], [file_path]
+    ):
+        document_id = info.document_id
+    assert document_id
 
 
 async def test_delete_collection(shabti_client):
