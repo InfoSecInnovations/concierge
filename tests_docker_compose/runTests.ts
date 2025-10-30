7199dd99d12f426cb68c6a31f277f1fefbd43a58
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-10-30 19:54:13 +0100

    wrap test runner in try catch to be able to run the final report at the end instead of exiting

--- tests_docker_compose/runTests.ts
+++ tests_docker_compose/runTests.ts
@@ -10,47 +10,49 @@ import xunitViewer from "xunit-viewer";
 
 await rm("./test_results", { recursive: true, force: true });
 
-console.log(`
------------------------------------------------------
-   RUNNING TESTS FOR SECURITY DISABLED INSTANCE...
------------------------------------------------------
-`);
+try {
+	console.log(`
+	-----------------------------------------------------
+		RUNNING TESTS FOR SECURITY DISABLED INSTANCE...
+	-----------------------------------------------------
+	`);
 
-dotenv.config({ path: "security-disabled-env", override: true, quiet: true });
-await nukeExisting();
-console.log(
-	"building Docker Compose... (can take some time if there are updates to the dependencies)",
-);
-await $`docker compose --env-file security-disabled-env -f ./docker-compose-pytest.yml build`.quiet();
-console.log(`
-____________RUNNING PYTHON TESTS______________
-`);
-await $`docker compose --env-file security-disabled-env -f ./docker-compose-pytest.yml up --attach shabti`;
-console.log(
-	"building Docker Compose... (can take some time if there are updates to the dependencies)",
-);
-await $`docker compose --env-file security-disabled-env -f ./docker-compose-client-test.yml build`.quiet();
-await $`docker compose --env-file security-disabled-env -f ./docker-compose-client-test.yml up --attach shabti-client`;
-// launch API
-await $`docker compose --env-file security-disabled-env build`.quiet();
-await $`docker compose --env-file security-disabled-env up -d`;
-console.log(`
-__________RUNNING NODE TESTS___________
-`);
-console.log("waiting for API service to launch...");
-while (true) {
-	try {
-		await fetch("http://localhost:15131");
-		break;
-	} catch {
-		continue;
+	dotenv.config({ path: "security-disabled-env", override: true, quiet: true });
+	await nukeExisting();
+	console.log(
+		"building Docker Compose... (can take some time if there are updates to the dependencies)",
+	);
+	await $`docker compose --env-file security-disabled-env -f ./docker-compose-pytest.yml build`.quiet();
+	console.log(`
+	____________RUNNING PYTHON TESTS______________
+	`);
+	await $`docker compose --env-file security-disabled-env -f ./docker-compose-pytest.yml up --attach shabti`;
+	console.log(
+		"building Docker Compose... (can take some time if there are updates to the dependencies)",
+	);
+	await $`docker compose --env-file security-disabled-env -f ./docker-compose-client-test.yml build`.quiet();
+	await $`docker compose --env-file security-disabled-env -f ./docker-compose-client-test.yml up --attach shabti-client`;
+	// launch API
+	await $`docker compose --env-file security-disabled-env build`.quiet();
+	await $`docker compose --env-file security-disabled-env up -d`;
+	console.log(`
+	__________RUNNING NODE TESTS___________
+	`);
+	console.log("waiting for API service to launch...");
+	while (true) {
+		try {
+			await fetch("http://localhost:15131");
+			break;
+		} catch {
+			continue;
+		}
 	}
-}
-await $`bun test --reporter=junit --reporter-outfile=./tests_docker_compose/test_results/bun_security_disabled.xml`
-	.cwd("..")
-	.env({ ...process.env, FORCE_COLOR: "1" });
+	await $`bun test --reporter=junit --reporter-outfile=./tests_docker_compose/test_results/bun_security_disabled.xml`
+		.cwd("..")
+		.env({ ...process.env, FORCE_COLOR: "1" });
 
-await runSecurityTests();
+	await runSecurityTests();
+} catch {}
 
 await mkdir("./processed_test_runs", { recursive: true });
 const filename = `shabti_test_run_${new Date().toISOString().replace(/:/g, "_")}`;
