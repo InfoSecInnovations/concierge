efd56db6d2df2acb99cf60c83546404e24943111
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2025-11-05 18:41:16 +0100

    remove unused code from lib file

--- docker_containers/shabti_api/tests/security_enabled/lib.py
+++ docker_containers/shabti_api/tests/security_enabled/lib.py
@@ -1,19 +1,9 @@
 from shabti_keycloak import (
     get_keycloak_client,
-    get_keycloak_admin_openid_token,
 )
 from ...src.app.document_collections import (
     create_collection,
-    delete_collection,
-    get_collections,
 )
-from ...src.app.ingesting import insert_document
-from ...src.app.loading import load_file
-import os
-
-
-# track all created collection IDs here (some of the lookup may get overwritten)
-collection_ids = []
 
 
 async def create_collection_for_user(user, location, collection_name):
@@ -22,31 +12,4 @@ async def create_collection_for_user(user, location, collection_name):
     collection_info = await create_collection(
         token["access_token"], collection_name, location
     )
-    collection_ids.append(collection_info.collection_id)
     return collection_info.collection_id
-
-
-filename = "test_doc.txt"
-file_path = os.path.join(os.path.dirname(__file__), "..", "assets", filename)
-# we will use the same file for each test
-
-with open(file_path, "rb") as f:
-    doc = load_file(f, filename)
-    binary = f.read()
-
-
-async def ingest_document(user, collection_id):
-    keycloak_client = get_keycloak_client()
-    token = keycloak_client.token(user, "test")
-    async for ingest_info in insert_document(
-        token["access_token"], collection_id, doc, binary
-    ):
-        pass
-    return ingest_info.document_id
-
-
-async def clean_up_collections():
-    token = get_keycloak_admin_openid_token()
-    collections = await get_collections(token["access_token"])
-    for collection in collections:
-        await delete_collection(token["access_token"], collection.collection_id)
