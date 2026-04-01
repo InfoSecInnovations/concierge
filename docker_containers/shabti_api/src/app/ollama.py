adf2a0d3e716a450a3275cef50710db7e4e265dc
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2026-04-01 21:23:17 +0200

    attempt to get FastEmbed to work with GPU (so far unsuccessfully)

--- docker_containers/shabti_api/src/app/ollama.py
+++ docker_containers/shabti_api/src/app/ollama.py
@@ -9,38 +9,39 @@ def host():
     return os.getenv("OLLAMA_HOST", "localhost")
 
 
+def do_loading(model_name):
+    def is_loaded():
+        models = requests.get(f"http://{host()}:11434/api/tags")
+        model_list = json.loads(models.text)["models"]
+        return next(
+            filter(lambda x: x["name"].split(":")[0] == model_name, model_list),
+            None,
+        )
+
+    while not is_loaded():
+        print(f"{model_name} model not found. Please wait while it loads.")
+        request = requests.post(
+            f"http://{host()}:11434/api/pull",
+            data=json.dumps({"name": model_name}),
+            stream=True,
+        )
+        current = 0
+        for item in request.iter_lines():
+            if item:
+                value = json.loads(item)
+                # TODO: display statuses
+                if "total" in value:
+                    if "completed" in value:
+                        current = value["completed"]
+                    yield ModelLoadInfo(
+                        progress=current,
+                        total=value["total"],
+                        model_name=model_name,
+                    )
+
+
 def load_model(model_name):
-    def do_loading():
-        def is_loaded():
-            models = requests.get(f"http://{host()}:11434/api/tags")
-            model_list = json.loads(models.text)["models"]
-            return next(
-                filter(lambda x: x["name"].split(":")[0] == model_name, model_list),
-                None,
-            )
-
-        while not is_loaded():
-            print(f"{model_name} model not found. Please wait while it loads.")
-            request = requests.post(
-                f"http://{host()}:11434/api/pull",
-                data=json.dumps({"name": model_name}),
-                stream=True,
-            )
-            current = 0
-            for item in request.iter_lines():
-                if item:
-                    value = json.loads(item)
-                    # TODO: display statuses
-                    if "total" in value:
-                        if "completed" in value:
-                            current = value["completed"]
-                        yield ModelLoadInfo(
-                            progress=current,
-                            total=value["total"],
-                            model_name=model_name,
-                        )
-
-    return StreamingResponse(do_loading())
+    return StreamingResponse(do_loading(model_name))
 
 
 def create_embeddings_ollama(text):
