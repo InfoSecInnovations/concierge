95eef424ce4cf2ae437eeb76bf581ee6740f698d
sebovzeoueb <7989595+sebovzeoueb@users.noreply.github.com>
2026-08-08 13:53:11 +0200

    remove unnecessary text from model management area

--- shabti_configurator/server/modelManagementForm.tsx
+++ shabti_configurator/server/modelManagementForm.tsx
@@ -22,24 +22,13 @@ export const ModelManagementForm = async () => {
 					defaultModel={selection.defaultModel}
 				>
 					The language models which will be available to users when querying
-					Shabti. Selecting a model which isn't installed yet will download it,
-					and deselecting one removes it from Shabti.
+					Shabti.
 				</ChatModelSelector>
 				<p>
-					The embeddings model in use is{" "}
-					<strong>{selection.embeddingsModel}</strong>, which can only be
-					changed by reinstalling as changing it would invalidate all existing
-					ingested documents.
-				</p>
-				<p>
-					Removing a model doesn't delete the downloaded files, so you can add
-					it back again without waiting for another download. If you want to
-					reclaim the disk space you will have to remove the Llama.cpp service,
-					which deletes all of the downloaded models.
-				</p>
-				<p>
-					Applying changes restarts the LLM service, so Shabti will be unable to
-					answer queries for a short while.
+					<label>Current Embeddings Model</label>{" "}
+					<strong>{selection.embeddingsModel}</strong>. You cannot change the
+					embeddings model as this would invalidate all currently ingested
+					documents.
 				</p>
 				<button type="submit">Apply Model Changes</button>
 			</fieldset>
